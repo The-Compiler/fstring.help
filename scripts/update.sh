a5b329f419885d07ef60c0936c3e5d36a190a5d5
Florian Bruhin <me@the-compiler.org>
2022-04-30 10:22:03 +0200

    Fix copying cheat directory

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,4 +3,4 @@ cd ~/proj/fstring.help/
 source .venv/bin/activate
 git pull --quiet --ff-only
 scripts/build.sh
-cp -R img index.html /var/www/virtual/bruhinsw/fstring.help/
+cp -R img index.html cheat /var/www/virtual/bruhinsw/fstring.help/
